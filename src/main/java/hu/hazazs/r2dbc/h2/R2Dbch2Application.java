0a572d9cafb957a003d6aa9c0258d39d6a7c2120
hazazs <hazafi.tibor@gmail.com>
2025-10-21 20:37:25 +0200

    Initial commit

---
+++ src/main/java/hu/hazazs/r2dbc/h2/R2Dbch2Application.java
@@ -0,0 +1,13 @@
+package hu.hazazs.r2dbc.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2Dbch2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2Dbch2Application.class, args);
+	}
+
+}
---
+++ src/test/java/hu/hazazs/r2dbc/h2/R2Dbch2ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.hazazs.r2dbc.h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2Dbch2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
